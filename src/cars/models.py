baa131c5c21d2be5fadf2f1536887d030d0fe220
Mehdi <mehdi@gmail.com>
2024-07-18 08:58:15 +0200

    Création des Sérializers

--- src/cars/models.py
+++ src/cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    immatriculation = models.CharField()
+    marque = models.CharField()
+    modele = models.CharField()
+    etat = models.CharField()
+
